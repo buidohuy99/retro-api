f81dbf415fb0f61ee2df99896fbcc897c93f71ab
buidohuy99 <sunnyhill93@gmail.com>
2020-10-26 18:11:27 +0700

    changed failure message

--- app.js
+++ app.js
@@ -89,8 +89,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).send({info: "sorry, we can't understand this"});
 });
 
 module.exports = app;
